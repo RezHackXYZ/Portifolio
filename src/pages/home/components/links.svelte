cd9fad67959907eedf23e63cae6e9256f754c3b4
RezHackXYZ <rhythmupa@gmail.com>
2025-03-21 18:03:27 +0530

    fdfds

--- src/pages/home/components/links.svelte
+++ src/pages/home/components/links.svelte
@@ -2,6 +2,16 @@
 	import Link from "./link.svelte";
 </script>
 
-<div>
+<div id="root">
 	<Link link="#/aboutme" text="About Me" />
 </div>
+
+<style>
+	#root {
+		display: flex;
+		background-color: #0f0f0f;
+		padding: 10px;
+		margin: 10px;
+		border-radius: 30px;
+	}
+</style>
