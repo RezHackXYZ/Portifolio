56fed333f13f070a9797e7c81a6d29b7d14340d4
RezHackXYZ <rhythmupa@gmail.com>
2025-03-25 10:04:06 +0530

    sdffds

--- src/app.svelte
+++ src/app.svelte
@@ -30,6 +30,7 @@
 		height: 100%;
 		display: flex;
 		flex-direction: column;
+		margin: 0;
 	}
 
 	#nav {
--- src/pages/home/home.svelte
+++ src/pages/home/home.svelte
@@ -3,20 +3,23 @@
 </script>
 
 <div id="root">
-	<div id="me">
-		<img
-			src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/0ec4bfb7eea37c2d45276ce04276da40533b6d05_group_1__1_.svg"
-			alt="my pfp"
-			id="pfp"
-		/>
-		<div id="text">
-			<h1 id="YoHeading">Yo, Im RezHackXYZ!</h1>
-			<h5 id="boutMe">
-				Im a 12 year old kid who loves tech and is learing web dev!!!!!
-			</h5>
+	<div>
+		<div id="me">
+			<img
+				src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/0ec4bfb7eea37c2d45276ce04276da40533b6d05_group_1__1_.svg"
+				alt="my pfp"
+				id="pfp"
+			/>
+			<div id="text">
+				<h1 id="YoHeading">Yo, Im RezHackXYZ!</h1>
+				<h5 id="boutMe">
+					Im a 12 year old kid who loves tech and is learing web
+					dev!!!!!
+				</h5>
+			</div>
 		</div>
+		<div id="links"><Links /></div>
 	</div>
-	<div id="links"><Links /></div>
 </div>
 
 <style>
@@ -26,17 +29,13 @@
 		align-items: center;
 		justify-content: center;
 		height: 100%;
-		width: calc(100vw - 40px);
-		margin: 20px;
+		margin: 0px;
 	}
 
 	#me {
 		display: flex;
-		align-items: center;
-		background-color: hsla(0, 0%, 12%, 0.5);
-		padding: 15px;
-		border-radius: 5px;
 		margin: 10px;
+		margin-bottom: 20px;
 	}
 	@media screen and (max-width: 700px) {
 		#me {
@@ -45,12 +44,8 @@
 	}
 
 	#links {
-		display: flex;
-		align-items: center;
-		background-color: hsla(0, 0%, 12%, 0.5);
-		padding: 15px;
-		border-radius: 5px;
 		margin: 10px;
+		width: fit-content;
 	}
 
 	#YoHeading {
